be15729d2d7a188b50ed2c6e8839e6bc8aa0a057
chrisleboa <cleboa@stanford.edu>
2019-08-05 16:55:03 +0545

    adding to R

---
+++ Github and Tidyverse/CC-Ghent-master/README.md
@@ -0,0 +1,13 @@
+# CC-Ghent
+This repository holds the materials for the Coding Club workshop we organised with the EVENET network in Ghent in Feb 2018.
+
+Living Planet Index data used as part of the tutorial can be accessed here: http://www.livingplanetindex.org/home/index 
+
+We would love to hear your feedback on the tutorial, whether you did it at a Coding Club workshop or online: 
+https://www.surveymonkey.com/r/XD85MW5
+
+Check out https://ourcodingclub.github.io/workshop/ to learn how you can get involved!
+
+This work is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/).
+
+[![License: CC BY-SA 4.0](https://licensebuttons.net/l/by-sa/4.0/80x15.png)](https://creativecommons.org/licenses/by-sa/4.0/)
---
+++ Github and Tidyverse/CC-Ghent-master/tutorial_script.R
@@ -0,0 +1,41 @@
+# GOAL: To visualise plant trait correlations and make a multi-plot panel of summary statistics
+
+# Raw data: Measurements of several plant functional traits 
+
+# (SLA, Leaf area, Leaf fresh mass, and Leaf dry mass) 
+# on multiple individuals of four different species
+
+# Get data
+load("traits.RData")
+load("traits_sum.RData")
+
+# Load libraries
+library(corrplot)
+library(ggplot2)
+
+# Comparing within-individual correlations
+# Trait-trait correlation plot
+(correlation <- corrplot(cor(traits[,2:5], use = "pairwise.complete.obs")))
+
+# Save the plot in your working directory
+png(filename = "trait_correlation.png", width = 600, height = 600)
+(correlation <- corrplot(cor(traits[,2:5], use = "pairwise.complete.obs")))
+dev.off()
+
+# Transform dataset to long format
+traits_long <- gather(traits, Trait, value, select = 2:5)
+
+# Graph raw trait data behind mean +/- 95% CI's and save the file
+(trait.plot <- ggplot()+
+    geom_point(data = traits_long, mapping = aes(x = SpeciesName, y = value, colour = Trait), alpha = 0.1) +
+    geom_errorbar(data = traits_sum, mapping = aes(x = SpeciesName, ymin = q2.5, ymax = q97.5, group = Trait), width = 0.3) +
+    geom_point(data = traits_sum, mapping = aes(x = SpeciesName, y = mean, group = Trait), size = 4, colour = "black") +
+    geom_point(data = traits_sum, mapping = aes(x = SpeciesName, y = mean, colour = Trait), size = 3) +
+    facet_wrap(~Trait, scales = "free_y")+
+    theme_classic() +
+    scale_x_discrete(labels = c("Dryas", "Eriophorum", "Oxyria", "Salix")) +
+    ylab("Trait Value") +
+    xlab("Species"))
+
+# We can save plots made using ggplot2 with ggsave, which is just one line of code
+ggsave(trait.plot, filename = "traits.png", height = 5, width = 10)
